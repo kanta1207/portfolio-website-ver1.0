2510e4672cc647804d1e158219fc46838c93137e
Kanta Sakai <99339182+kanta1207@users.noreply.github.com>
2022-10-16 16:16:44 +0900

    first commit

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import React from "react";
+
+export const Layout = () => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      
+    </>
+  );
+};
